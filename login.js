debfc17c60dd838fcdf87e02b8048564d3680427
Ahmet Kütük <64685481+ack8203@users.noreply.github.com>
2020-05-17 20:08:05 +0300

    Textbox animation

---
+++ login.js
@@ -0,0 +1,23 @@
+function wi() {
+    var y =document.getElementById("texts2");
+    y.style.width="300px"
+
+    var x = document.getElementById("texts1");
+    x.style.width="320px"
+
+}
+
+function wi3() {
+    var y =document.getElementById("texts1");
+    y.style.width="300px"
+    var x = document.getElementById("texts2");
+    x.style.width="320px"
+
+
+}
+function wi4() {
+    var y =document.getElementById("texts1");
+    y.style.width="300px"
+    var x = document.getElementById("texts2");
+    x.style.width="300px"
+}
